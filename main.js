3f076c1de97175a23672ba7ecc2cf6269b142d42
PelleHanspers <p.hanspers@gmail.com>
2023-12-20 11:20:11 +0100

    uppdatera fetch-funktionen + kommentering

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// deklarera globala element
 let postSection = document.getElementById('post-section');
 let container = document.getElementById('container');
 let newPost = document.getElementById('new-post');
@@ -7,33 +8,35 @@ let showButton = document.createElement('button');
 showButton.setAttribute('id', 'addButton');
 newPost.append(showButton);
 showButton.innerHTML = "<strong>New Post</strong>";
-
+// kör fetch-funktionen bara local storage är tomt
 if (localStorage.getItem('posts') == null) {
     console.log('local storage is empty');
     fetchData();
 }
-
+// skriver ut local storage på sidan
 renderLocalStorage();
-
+//hämtar data remote
 function fetchData() {
     console.log('fetch');
+    //skapar och sparar tom array till local storage för att sen kunna populera med hämtad data
     let localArray = [];
     localStorage.setItem('posts', JSON.stringify(localArray));
-    posts =  fetch('https://dummyjson.com/posts')
+    posts = fetch('https://dummyjson.com/posts')
         .then(res => res.json())
-        .then((res) => setLocalStorage(res.posts));
-        //console.log(posts); 
+        //lägger till property .voted till objekt i array
+        .then((res) => setLocalStorage(res.posts.map(post => {
+            post.voted = false;
+            return post;
+        })));
 }
-
+// tar fetch-data och bryter ut obekten ur array och skickar separerade till till nästa funktion
 function setLocalStorage(posts) {
     for (let i = 0; i < posts.length; i++) {
         let post = posts[i];
-        post.voted = false;
         addToLocalStorage(post);
-        console.log(post);
     };
 }
-
+// deklarera globala element för att skapa nytt inlägg
 let maxId = 0;
 
 let postButton = document.createElement("button");
@@ -62,11 +65,11 @@ let buttonSpan = document.createElement('span');
 buttonSpan.setAttribute('id', 'buttonSpan');
 let myForm = document.createElement('form');
 myForm.setAttribute('id', 'myForm');
-
+// evenlisteners för nya inlägg
 showButton.addEventListener("click", showNewPost);
 
 postButton.addEventListener("click", addNewPost);
-
+// tar bort och tömmer element för nya inlägg
 cancelButton.addEventListener('click', function() {
     addDiv.remove();
     legend.remove();
@@ -81,6 +84,7 @@ cancelButton.addEventListener('click', function() {
     showButton.hidden = false;
 } )
 
+// visar fält för nytt inlägg
 function showNewPost() {
     newPost.append(addDiv);
     addDiv.append(legend);
@@ -94,6 +98,8 @@ function showNewPost() {
     showButton.hidden = true;
 }
 
+let inputArray = [];
+// skapar nytt inlägg som sparas i local storage
 function addNewPost() {
     let x = document.getElementById("addTitle").value;
     let y = document.getElementById("addBody").value;
@@ -102,7 +108,7 @@ function addNewPost() {
     let tagArray = z.split(/[ ,]+/).slice(0, 3);
 
     let temp = document.querySelectorAll('.article');
-
+    // ser till att varje nytt inlägg får ett id av senaste inlägg +1
     temp.forEach(p => {
         if (p.postsId > maxId) {
             maxId = p.postsId;
@@ -120,8 +126,7 @@ function addNewPost() {
         reactions: 0,
         voted: false,
     }
-
-//    console.log(newPostObj.tags);   
+    // reset element 
     localArray = [];
     tagArray = [];
     addDiv.remove();
@@ -138,94 +143,93 @@ function addNewPost() {
 
     addToLocalStorage(newPostObj);
 };
-
+// lägger till inlägg, både initiellt från fetch och nya inlägg från användare
 function addToLocalStorage(post) {
-    //console.log(post);
     let localArray = JSON.parse(localStorage.getItem('posts'));
-//    console.log(localArray);
     
     localArray.push(post);
     localStorage.setItem('posts', JSON.stringify(localArray));
 
     renderLocalStorage();
 }
-
+// skriver ut inlägg på sidan
 function renderLocalStorage() {
-    localArray = JSON.parse(localStorage.getItem('posts'));
-      for (let i = 0; i < localArray.length; i++) {
-          let post = localArray[i];
-          
-          let article = document.createElement('article');
-          article.classList.add('article');
-  
-          let title = document.createElement('h3');
-          title.innerText = post.title;
-          title.classList.add("title");
-  
-          let tag1 = document.createElement('a');
-          tag1.innerText = post.tags[0];
-          let tag2 = document.createElement('a');
-          tag2.innerText = post.tags[1];
-          let tag3 = document.createElement('a');
-          tag3.innerText = post.tags[2];
-  
-          let user = document.createElement('span');
-          user.innerText = "User : " + post.userId;
-          user.classList.add('user');
-  
-          let body = document.createElement('p');
-          body.innerText = post.body;
-          body.classList.add('body');
-  
-          let reaction = document.createElement("span");
-          reaction.classList.add("reaction");
-  
-          let reactButton = document.createElement("button");
-          reaction.append(reactButton);
-          reactButton.classList.add("reactButton");
-  
-          let icon = document.createElement("i");
-          reactButton.appendChild(icon);
-          icon.classList.add("fa-regular");
-          icon.classList.add("fa-thumbs-up");
-  
-          let count = document.createElement("span");
-          count.innerText = post.reactions;
-          reaction.append(count);
-          count.num = post.reactions;
-          count.classList.add("count");
-          
-          let number = document.createElement("span");
-          number.innerText = "Post: " + post.id;
-          number.classList.add("number");
-  
-          article.postsId = post.id;
-          article.id = 'article-' + post.id;
-  
-          reactButton.addEventListener('click', function () {
-  
-              if (post.voted) return;
-              count.innerHTML = ++count.num;
-              post.voted = true;
-              let localArray = JSON.parse(localStorage.getItem('posts'));
-  
-              localArray.forEach(p => {
-                  if (p.id == post.id) {
-                      p.reactions = count.num;
-                      p.voted = post.voted;
-                  }
-              })
-              localStorage.setItem('posts', JSON.stringify(localArray));
-          })
-  
-          article.append(title);
-          for (let i = 0; i < post.tags.length; i++) {
-              let tag1 = document.createElement('a');
-              tag1.innerText = post.tags[i]
-              article.append(tag1);
-          }
-  
-          article.append(user, body, reaction, number);
-          postSection.append(article);
-      }
-  }
+localArray = JSON.parse(localStorage.getItem('posts'));
+    for (let i = 0; i < localArray.length; i++) {
+        let post = localArray[i];
+        
+        let article = document.createElement('article');
+        article.classList.add('article');
+
+        let title = document.createElement('h3');
+        title.innerText = post.title;
+        title.classList.add("title");
+
+        let tag1 = document.createElement('a');
+        tag1.innerText = post.tags[0];
+        let tag2 = document.createElement('a');
+        tag2.innerText = post.tags[1];
+        let tag3 = document.createElement('a');
+        tag3.innerText = post.tags[2];
+
+        let user = document.createElement('span');
+        user.innerText = "User : " + post.userId;
+        user.classList.add('user');
+
+        let body = document.createElement('p');
+        body.innerText = post.body;
+        body.classList.add('body');
+
+        let reaction = document.createElement("span");
+        reaction.classList.add("reaction");
+
+        let reactButton = document.createElement("button");
+        reaction.append(reactButton);
+        reactButton.classList.add("reactButton");
+
+        let icon = document.createElement("i");
+        reactButton.appendChild(icon);
+        icon.classList.add("fa-regular");
+        icon.classList.add("fa-thumbs-up");
+
+        let count = document.createElement("span");
+        count.innerText = post.reactions;
+        reaction.append(count);
+        count.num = post.reactions;
+        count.classList.add("count");
+        
+        let number = document.createElement("span");
+        number.innerText = "Post: " + post.id;
+        number.classList.add("number");
+
+        article.postsId = post.id;
+        article.id = 'article-' + post.id;
+        // hantera like's
+        reactButton.addEventListener('click', function () {
+            // hanterar att man inte kan like'a samma inlägg flera ggr
+            if (post.voted) return;
+            count.innerHTML = ++count.num;
+            post.voted = true;
+            // hämtar data i local storage och sparar om med uppdaterade värden
+            let localArray = JSON.parse(localStorage.getItem('posts'));
+
+            localArray.forEach(p => {
+                if (p.id == post.id) {
+                    p.reactions = count.num;
+                    p.voted = post.voted;
+                }
+            })
+            localStorage.setItem('posts', JSON.stringify(localArray));
+        })
+        // append element i korrekt ordning för varje inlägg och lägger till alla inlägg i huvudsektionen
+        article.append(title);
+        for (let i = 0; i < post.tags.length; i++) {
+            let tag1 = document.createElement('a');
+            tag1.innerText = post.tags[i]
+            article.append(tag1);
+        }
+
+        article.append(user, body, reaction, number);
+        postSection.append(article);
+    }
+}
