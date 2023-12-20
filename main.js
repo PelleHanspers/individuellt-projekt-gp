56b46623ccbc922dcb773ab5d2404eb832dc6174
PelleHanspers <p.hanspers@gmail.com>
2023-12-20 09:00:52 +0100

    fetch funktion

--- main.js
+++ main.js
@@ -7,3 +7,23 @@ let showButton = document.createElement('button');
 showButton.setAttribute('id', 'addButton');
 newPost.append(showButton);
 showButton.innerHTML = "<strong>New Post</strong>";
+
+function fetchData() {
+    console.log('fetch');
+    let localArray = [];
+    localStorage.setItem('posts', JSON.stringify(localArray));
+    posts =  fetch('https://dummyjson.com/posts')
+        .then(res => res.json())
+        .then((res) => setLocalStorage(res.posts));
+        //console.log(posts); 
+}
+
+function setLocalStorage(posts) {
+    for (let i = 0; i < posts.length; i++) {
+        let post = posts[i];
+        post.voted = false;
+        //addToLocalStorage(post);
+        console.log(post);
+    };
+}
+fetchData();
